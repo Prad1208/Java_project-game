86f19d7c99924075629e32098e31c0d84aa1135a
Praduman <pradumandwivedi1208@gmail.com>
2026-06-18 22:28:16 +0530

    Initial

---
+++ browserhistorysimulator/browserhistorysimulator.java
@@ -0,0 +1,22 @@
+package browserhistorysimulator;
+import java.util.*;
+public class browserhistorysimulator {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        LinkedList<String> ll=new LinkedList<>();
+        ListIterator<String> litr=ll.listIterator();
+        System.out.println("*************************");
+        System.out.println("Mini Project - Browser History Simulation");
+System.out.println("\n---------Browser Menu--------");
+System.out.println("1.Visit new Page");
+System.out.println("2.Go Back");
+System.out.println("3.Go Forward");
+System.out.println("4.Show Current Page");
+System.out.println("5.Exit");
+System.out.println("Enter your Choice");
+int choice =sc.nextInt();
+switch(choice){
+    
+}
+    }
+}
